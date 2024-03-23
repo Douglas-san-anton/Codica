8af96cf5e7a1199eadcc5f9ac35d6fc3dcc426bc
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2024-03-23 18:00:14 -0300

    Completed tasks number 7

---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+import run, { roundsCount } from '../index.js';
+import { generateNumber } from '../utils.js';
+
+const description = 'Encuentra el máximo común divisor de los números dados.';
+
+const gcd = (x, y) => (y === 0 ? x : gcd(y, x % y));
+
+const generateRound = () => {
+    const number1 = generateNumber(1, 30);
+    const number2 = generateNumber(1, 30);
+
+    const question = `${number1} ${number2}`;
+    const answer = String(gcd(number1, number2));
+
+    return [question, answer];
+};
+
+export default () => {
+    const rounds = [];
+    for (let i = 0; i < roundsCount; i += 1) {
+        rounds.push(generateRound());
+    }
+
+    run(description, rounds);
+};
